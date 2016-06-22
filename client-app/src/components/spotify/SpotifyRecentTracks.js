5d5425bcfc30f33e8f947f6d54c9e64ce01f3a31
Joseph Krump <krump.joseph@gmail.com>
2016-06-22 11:52:13 -0700

    polling every 5 seconds for updates to recent tracks

--- client-app/src/components/spotify/SpotifyRecentTracks.js
+++ client-app/src/components/spotify/SpotifyRecentTracks.js
@@ -9,14 +9,18 @@ export default class SpotifyRecentTracks extends Component {
       currentTracks: []
     }
   }
-  componentWillMount(){
-    let uri = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${scrobblerConfig.SCROBBLER_API_REGISTERED_USER}&api_key=${scrobblerConfig.SCROBBLER_API_KEY}&format=json`;
+  loadRecentTracks(){
+    let uri = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${scrobblerConfig.SCROBBLER_API_REGISTERED_USER}&api_key=${scrobblerConfig.SCROBBLER_API_KEY}&limit=5&format=json`;
     let recentTrackComponents;
 
     $.getJSON(uri, function(data) {
-      console.log(data.recenttracks.track);
+      console.log('Getting Tracks');
       this.setState({currentTracks: data.recenttracks.track});
     }.bind(this));
+  }
+  componentDidMount(){
+    this.loadRecentTracks();
+    setInterval(this.loadRecentTracks.bind(this), 5000);
   }
   render() {
     let recentTrackComponents = this.state.currentTracks.map((track, i) => {
